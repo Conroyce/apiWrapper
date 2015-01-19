bed64f4854b45908c3175a3e2e470cd76b5d27c0
Connor Kojimoto <cokojimoto@gmail.com>
2015-01-19 11:20:10 -0800

    completed step 5 of api wrapper

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ var FileApi = require('./fileWriter');
 var myFs = new FileApi();
 myFs.write('test.txt','test data');
 myFs.append("test.txt"," I'm adding new Content!");
+myFs.print("test.txt","utf-8");
--- fileWriter.js
+++ fileWriter.js
@@ -4,7 +4,16 @@ module.exports = function() {
   this.write = function(name,data) {
     return fs.writeFileSync(name,data);
   };
+
   this.append = function(name,data) {
     return fs.appendFileSync(name,data);
   };
+
+  this.print = function(file,opt) {
+    return fs.readFile(file,opt,function(err,data) {
+      if(err) throw err;
+      console.log(data);
+    });
+  };
+
 };
